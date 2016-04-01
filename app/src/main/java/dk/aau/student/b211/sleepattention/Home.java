4297f7b8e4d8ec9a5fe51acbfa71c36d3bd510b7
MadsDahl92 <Mads.dahl92@gmail.com>
2016-04-01 12:08:29 +0200

    Mads Test

--- app/src/main/java/dk/aau/student/b211/sleepattention/Home.java
+++ app/src/main/java/dk/aau/student/b211/sleepattention/Home.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
 
-    int test;
+    int test = 0;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        test = 1;
 
     }
 }

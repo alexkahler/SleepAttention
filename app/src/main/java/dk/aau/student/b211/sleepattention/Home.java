320225af44136de5c672507ba08fbbe373eada5a
alexkahler <akahle15@student.aau.dk>
2016-04-01 11:57:40 +0200

    Initial commit

---
+++ app/src/main/java/dk/aau/student/b211/sleepattention/Home.java
@@ -0,0 +1,13 @@
+package dk.aau.student.b211.sleepattention;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
